4d9a47b8266ab38713465520045589aa2d7fcd0f
GabrielMaharaj <92026216+GabrielMaharaj@users.noreply.github.com>
2022-03-23 23:48:09 +0000

    added "yo" upon spikeBall collision

--- src/game/SpikeBall.java
+++ src/game/SpikeBall.java
@@ -4,9 +4,24 @@ package game;
 import city.cs.engine.*;
 import org.jbox2d.common.Vec2;
 
-public class SpikeBall extends Walker {
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
 
+
+public class SpikeBall extends Walker {
+    private static SoundClip yo;
     int spikeBallWalkingSpeed =3;
+    static {
+        try {
+            yo = new SoundClip("data/shaqYo.mp3");   // Open an audio input stream
+            //  boom.play();                              // Set it to  play
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            //code in here will deal with any errors
+            //that might occur while loading/playing sound
+            System.out.println(e);
+        }
+    }
 
 
     // enemy health level declaration and methods
@@ -83,5 +98,11 @@ public class SpikeBall extends Walker {
     }
     public void reverseSpikeBallWalkingSpeed(){SpikeBallWalkingSpeed = SpikeBallWalkingSpeed*-1;}
 
+
+@Override
+    public void destroy(){
+        super.destroy();
+     yo.play();
+}
 
 }
