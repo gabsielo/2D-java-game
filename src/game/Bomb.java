e55d77779f1ee48dd49505e3b28b83c451d09c91
GabrielMaharaj <92026216+GabrielMaharaj@users.noreply.github.com>
2022-03-25 09:22:24 +0000

    added explosion image

---
+++ src/game/Bomb.java
@@ -0,0 +1,22 @@
+package game;
+
+import city.cs.engine.*;
+import org.jbox2d.common.Vec2;
+
+public class Bomb {
+   private static final BodyImage bombImage =
+     new BodyImage("data/unexplodedBomb.png", 4f);
+
+    private static final BodyImage explosion =
+            new BodyImage("data/explosion.png", 4f);
+
+
+
+    public Bomb(World world, Vec2 pos, int detonationTime) {
+
+
+
+
+
+    }
+}
